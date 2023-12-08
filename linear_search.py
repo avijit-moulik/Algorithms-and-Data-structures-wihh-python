fd95cb9a28f7e79c05750d890d3b9a680e8b1bbf
Avijit Moulik <137809579+avijit-moulik@users.noreply.github.com>
2023-12-08 14:22:46 +0600

    linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -12,3 +12,20 @@ def verify(index):
 numbers =[1,2,3,4,5,6,7,8,9,10]
 result = lenear_surch(numbers,3)     #the number you want to surch
 verify(result)    
+
+
+"""
+
+Time Complexity:
+
+    Worst-case: O(n) - In the worst-case scenario, where the element being searched is at the end of the list or not present at all, the algorithm may need to check every element in the list.
+    Best-case: O(1) - In the best-case scenario, where the target element is found at the beginning of the list, the algorithm will terminate after checking the first element.
+
+Space Complexity:
+
+    O(1) - The linear search algorithm has a constant space complexity because it uses a fixed amount of extra space regardless of the size of the input. It only requires a few variables (e.g., loop counters) and does not rely on additional data structures.
+
+In summary, the linear search algorithm has a linear time complexity O(n) in the worst case and constant space complexity O(1). It is a straightforward algorithm but may not be the most efficient for large datasets compared to more advanced search algorithms like binary search for sorted lists or hash-based methods.
+
+
+"""

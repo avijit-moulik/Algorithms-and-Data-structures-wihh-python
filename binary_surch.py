db32b0808d7dc2dde8f6d711157bac8cd189fc63
Avijit Moulik <137809579+avijit-moulik@users.noreply.github.com>
2023-12-08 13:49:48 +0600

    binary_surch.py

--- binary_surch.py
+++ binary_surch.py
@@ -1,13 +1,13 @@
-def binary_search(lst, target):
+def binary_search(list, target):
     first = 0
-    last = len(lst) - 1
+    last = len(list) - 1
     
     while first <= last:
         midpoint = (first + last) // 2
         
-        if lst[midpoint] == target:
+        if list[midpoint] == target:
             return midpoint
-        elif lst[midpoint] < target:
+        elif list[midpoint] < target:
             first = midpoint + 1
         else:
             last = midpoint - 1
